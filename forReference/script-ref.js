572b476b75e2fabcf2cc2aef9db0c21ad4840cd8
Fluffy <125940177+fluffywound27@users.noreply.github.com>
2023-05-07 23:22:07 -0500

    Add files via upload

--- forReference/script-ref.js
+++ forReference/script-ref.js
@@ -1,14 +1,23 @@
 const skipButtonElem = document.querySelector('.js-skip-button');
-
+const menuElem = document.querySelector('.js-menu')
+// 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('video', {
+  player = new YT.Player('player', {
+    height: '315',
+    width: '560',
+    videoId: 'yTDGs06o4eU',
+    playerVars: {
+      'playsinline': 1
+    },
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
@@ -16,19 +25,22 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
+// 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
   event.target.playVideo();
 }
 
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
 var done = false;
-
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
     done = true;
   }
-};
+}
 
 skipButtonElem.addEventListener('click', () => {
   player.seekTo(36);
+  menuElem.classList.remove('menu-vis')
 });
-
